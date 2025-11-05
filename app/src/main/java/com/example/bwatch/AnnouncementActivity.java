a7e2a2be2109ea977476fb5cbc13d05473eb5b8e
Demmar Romana <demmarromana38@gmail.com>
2025-11-05 23:02:36 +0800

    updated layout

---
+++ app/src/main/java/com/example/bwatch/AnnouncementActivity.java
@@ -0,0 +1,12 @@
+package com.example.bwatch;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AnnouncementActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.announcement); // your layout file
+    }
+}
